c402b11bb0eeacf1341ccb445a095d3573755638
Gennadij Yatskov <g.yatskov@fpi.contractors>
2020-04-18 18:58:50 +0200

    [src] Add background image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image'
 import logo from './logo.svg';
+import background from './background/20180517_123646.jpg'
 import './App.css';
 
 function App() {
@@ -7,17 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>yatskov.online</p>
       </header>
     </div>
   );
