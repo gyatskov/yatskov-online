aaac7e618873eecf5f60b913db091643d7da2dd1
Gennadij Yatskov <g.yatskov@fpi.contractors>
2020-04-18 23:57:33 +0200

    Updates

---
+++ precache-manifest.db4c99e03f5d770d28e4085fc6bc88f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fb90b0a70b7c30b992d05e8bf595dc",
+    "url": "/yatskov-online/index.html"
+  },
+  {
+    "revision": "d0a34936448d76d4b9d6",
+    "url": "/yatskov-online/static/css/main.4fe9f24c.chunk.css"
+  },
+  {
+    "revision": "430af7ea3418941bd970",
+    "url": "/yatskov-online/static/js/2.a0dad611.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/yatskov-online/static/js/2.a0dad611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a34936448d76d4b9d6",
+    "url": "/yatskov-online/static/js/main.18a7a261.chunk.js"
+  },
+  {
+    "revision": "8b1c31d4365a9a5da7e2",
+    "url": "/yatskov-online/static/js/runtime-main.b618525c.js"
+  },
+  {
+    "revision": "89d443db6e6151ea36eb6b0cfa9d8e2f",
+    "url": "/yatskov-online/static/media/20180517_123646.89d443db.jpg"
+  }
+]);
