0999b4410cbd97d2667e812422e09846f36544aa
Gennadij Yatskov <g.yatskov@fpi.contractors>
2020-04-29 23:37:11 +0200

    [src] Replace old layout with Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'react-bootstrap/Image'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import logo from './logo/logo_4x.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Image src={logo} className="App-logo" alt="logo" />
-        <p>yatskov.online</p>
-    <div class="site-wrapper">
-      <div class="site-wrapper-inner">
-        <div class="cover-container">
-          <div class="row">
-            <div class="col-md-3 pad">
-              <span class='socicon-linkedin icon'></span>
-            </div>
-            <div class="col-md-3 pad">
-              <span class='socicon-github icon'></span>
-            </div>
-            <div class="col-md-3 pad">
-              <span class='socicon-facebook icon'></span>
-            </div>
-            <div class="col-md-3 pad">
-            </div>
-          </div>
-        </div>
-      </div>
+      <Jumbotron>
+        <header className="App-header">
+          <Image src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Container className="Contact-row">
+          <Row>
+            <Col className="Contact-pad"><Button variant="dark" block>LinkedIn</Button></Col>
+            <Col className="Contact-pad"><Button variant="dark" block>E-Mail</Button></Col>
+            <Col className="Contact-pad"><Button variant="dark" block>XING</Button></Col>
+            <Col className="Contact-pad"><Button variant="dark" block>Github</Button></Col>
+          </Row>
+        </Container>
+      </Jumbotron>
     </div>
-      </header>
-  </div>
   );
 }
 
